6b8c47c9974a71ac30eae5b9c059449ef1b06e68
ehivan24 <ehivan24@hotmail.com>
2016-06-21 22:25:59 -0400

    New methods for analysis

--- data_analysis/employment_data_analysis/Data_Analysis_Utils.py
+++ data_analysis/employment_data_analysis/Data_Analysis_Utils.py
@@ -65,3 +65,44 @@ class Data_Analysis_Util(object):
         print("Standard Deviation: ", np.std(data))
         print("Median: ", np.median(data))
         print("Sum: ", np.sum(data))
+
+    """
+    Numpy
+    """
+    @staticmethod
+    def max_employment(countries, employment):
+        i = employment.argmax()
+        return (countries[i], employment[i])
+
+    @staticmethod
+    def min_employment(countries, employment):
+        i = employment.argmin()
+        return (countries[i], employment[i])
+
+
+    @staticmethod
+    def over_all_data(array1, array2):
+        return (array1 + array2) /2
+
+    @staticmethod
+    def standardized_data(values):
+        standarized_value = (values - values.mean()) / values.mean()
+        return standarized_value
+
+    @staticmethod
+    def mean_time_for_paid_students(time_spent, days_to_cancel):
+        return time_spent[days_to_cancel >= 7].mean()
+
+
+    @staticmethod
+    def variable_correlation(series1, series2):
+        both_above = (series1 > series1.mean()) & (series2 > series2.mean())
+        both_below = (series1 < series1.mean()) & (series2 < series2.mean())
+
+        is_same_direction = both_above | both_below
+        num_same_direction = is_same_direction.sum()
+        num_different_direction = len(series1) - num_same_direction
+
+        return (num_same_direction, num_different_direction)
+
+
--- data_analysis/employment_data_analysis/data_analysis_2_pandas_and_numpy.py
+++ data_analysis/employment_data_analysis/data_analysis_2_pandas_and_numpy.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from Data_Analysis_Utils import Data_Analysis_Util as util
+from Data_Analysis_Utils import Data_Analysis_Util as utils
 
 #life_expectancy.csv
 #employment_above_15.csv
@@ -14,12 +14,44 @@ from Data_Analysis_Utils import Data_Analysis_Util as util
 #male_completion_rate_rate.csv
 
 
-countries = pd.read_csv('male_completion_rate.csv')
-#enrollments = enrollments.values
-print countries[0]
+countries = pd.read_csv('gdp_per_capita.csv')
 
-for country in countries:
-    if country == 'Abkhazia':
-        print "Found"
-        break
+print("", len(countries['Country'].unique()))
+
+
+countries_array = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
+])
+
+employment = np.array([
+    55.70000076,  51.40000153,  50.5       ,  75.69999695,
+    58.40000153,  40.09999847,  61.5       ,  57.09999847,
+    60.90000153,  66.59999847,  60.40000153,  68.09999847,
+    66.90000153,  53.40000153,  48.59999847,  56.79999924,
+    71.59999847,  58.40000153,  70.40000153,  41.20000076
+])
+
+print "Maximum: ", utils.max_employment(countries_array, employment)
+print "Minimum: ", utils.min_employment(countries_array, employment)
+
+life_expectancy_values = [74.7, 75., 83.4, 57.6, 74.6, 75.4, 72.3, 81.5, 80.2,
+                          70.3, 72.1, 76.4, 68.1, 75.2, 69.8, 79.4, 70.8, 62.7,
+                          67.3, 70.6]
+
+gdp_values = [1681.61390973, 2155.48523109, 21495.80508273, 562.98768478,
+              13495.1274663, 9388.68852258, 1424.19056199, 24765.54890176,
+              27036.48733192, 1945.63754911, 21721.61840978, 13373.21993972,
+              483.97086804, 9783.98417323, 2253.46411147, 25034.66692293,
+              3680.91642923, 366.04496652, 1175.92638695, 1132.21387981]
+
+life_expectancy = pd.Series(life_expectancy_values)
+gdp = pd.Series(gdp_values)
+
+print "Life Expectancy \n", life_expectancy.describe()
+print "GDP \n", gdp.describe()
+print utils.variable_correlation(life_expectancy, gdp)
 
