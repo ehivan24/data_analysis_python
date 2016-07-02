20e589c327cc4ccd496bace9bc9de934ca17d1c2
ehivan24 <ehivan24@hotmail.com>
2016-07-01 23:29:51 -0400

    Predicting price

--- data_analysis/financial_analysis/pandas_4.py
+++ data_analysis/financial_analysis/pandas_4.py
@@ -21,26 +21,19 @@ def check_price(current_price, future_price):
 def moving_average(values):
     return np.mean(values)
 
-df_googl = pd.read_csv('NASDAQ_GOOGL.csv')
+df_googl = pd.read_csv('WIKI-FB.csv')
 """
-here it shifts the price to minus one.
+here the price shifts to minus one.
 """
 df_googl['Future'] = df_googl['Close'].shift(-1)
 df_googl.dropna(inplace=True)
-#print df_googl[['Future', 'Close']].head()
 
 df_googl['Predicted'] = list(map(check_price, df_googl['Close'], df_googl['Future']))
 
-#print df_googl['Price'].head()
-
-#df_googl['Mean'] = moving_average(df_googl['Close'])
-
-#print df_googl[['Open', 'High', 'Low', 'Close', 'Future', 'Predicted', 'Mean']].head()
-
-X = np.array(df_googl['Close'])
+X = np.array(df_googl.drop(['Date', 'Predicted'], 1))
 X = preprocessing.scale(X)
-
 print X
+
 y = np.array(df_googl['Predicted'])
 print y
 
@@ -48,10 +41,15 @@ x_train, x_test, y_train, y_test = cross_validation.train_test_split(X, y, test_
 
 classifier = svm.SVC(kernel='linear')
 classifier.fit(x_train, y_train)
+
 print classifier.score(x_test, y_test)
 
 
 
+#print df_googl[['Future', 'Close']].head()
+#print df_googl['Price'].head()
+#df_googl['Mean'] = moving_average(df_googl['Close'])
+#print df_googl[['Open', 'High', 'Low', 'Close', 'Future', 'Predicted', 'Mean']].head()
 #plt.plot(df_googl['Future'], '-', color='b')
 #plt.show()
 #print df_googl.head()
